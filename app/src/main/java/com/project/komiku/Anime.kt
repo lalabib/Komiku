9d56085915ef077fd5b263d81a2a0d26e76fbab2
Labib Hiba <57593172+lalabib@users.noreply.github.com>
2021-11-24 23:53:06 +0700

    fix variable

--- app/src/main/java/com/project/komiku/Anime.kt
+++ app/src/main/java/com/project/komiku/Anime.kt
@@ -1,9 +1,9 @@
 package com.project.komiku
 
 data class Anime (
-    var name: String = "",
-    var genre: String = "",
-    var photo: Int = 0,
-    var sinopsis: String = ""
+    var name: String,
+    var genre: String,
+    var photo: Int,
+    var sinopsis: String
 
 )
