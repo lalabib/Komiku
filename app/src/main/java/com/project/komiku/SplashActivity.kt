209b727af23970acd0c6c5579365b27b813bc119
Labib Hiba <57593172+lalabib@users.noreply.github.com>
2021-11-29 00:26:00 +0700

    Remove redudant space

--- app/src/main/java/com/project/komiku/SplashActivity.kt
+++ app/src/main/java/com/project/komiku/SplashActivity.kt
@@ -5,14 +5,11 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.Handler
 
-
-
 class SplashActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splashscreen)
-        
 
         val time:Long = 1300
         Handler(Looper.getMainLooper()).postDelayed({
